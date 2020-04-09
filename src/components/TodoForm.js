3cff3c0e4d581b6895d4602776d6c1a7d293d2dd
Ryan Smith <Franzferdinan51@gmail.com>
2020-04-09 18:03:40 -0400

    Add files via upload

---
+++ src/components/TodoForm.js
@@ -0,0 +1,44 @@
+import React from "react"
+import "../components/Todo.css";
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      itemName: ""
+    };
+  }
+
+  handleChanges = e => {
+
+    this.setState({ itemName: e.target.value });
+  };
+
+  // class property to submit form
+  handleAddItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.itemName);
+  };
+
+  render() {
+    console.log("rendering form");
+    return (
+      <form onSubmit={this.handleAddItem}>
+        <input
+          type="text"
+          name="item"
+          value={this.state.itemName}
+          onChange={this.handleChanges}
+          type="reset          "
+          defaultValue="Reset"
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+
+
+export default TodoForm;
